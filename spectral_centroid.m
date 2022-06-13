4711c202ef217f72309818f1ccd6c8a0f01356c0
floflita <floma2010@hotmail.de>
2022-06-13 10:52:04 +0200

    Spectral centroid defined using simplified definition, Mel Frequency Cepstral Coefficients

--- spectral_centroid.m
+++ spectral_centroid.m
@@ -13,9 +13,11 @@ function out = spectral_centroid(X,f)
 
 %% Run
 % combine all bins below 62.5 Hz into one band
-fn = cat(31.25, f(f>62.5));
-Xbelow = sum(X(f<62.5,:),1);
-Xn = cat(Xbelow, X(f>62.5,:));
+% fn = cat(31.25, f(f>62.5));
+% Xbelow = sum(X(f<62.5,:),1);
+% Xn = cat(Xbelow, X(f>62.5,:));
 
 
-out = sum(log2(fn/1000).*X^2,1) ./ sum(X^2);
+% out = sum(log2(f/1000).*abs(X).^2,1) ./ sum(abs(X).^2);
+
+out = sum(f.*abs(X),1) ./ sum(abs(X),1);
